49c28968806e0e16b4ca08ac06a54cc8c2a8b986
Lautaro Garcia <lautarojgarcia177@gmail.com>
2021-05-19 18:49:43 -0300

    Fin seccion 3

--- src/create.js
+++ src/create.js
@@ -1,9 +1,23 @@
 import { displayLog } from './utils';
+import { Observable } from 'rxjs';
 
 export default () => {
     /** start coding */
 
+    const hello = new Observable(function(observer) {
+        observer.next('Hello');
+        setTimeout(() => {observer.next('World');observer.complete();}, 2000);
+    });
 
+    const observer = {
+        next: evt => displayLog(evt),
+        error: err => console.err('[ERR] - ', err),
+        complete: () => displayLog('[DONE]')
+    };
+
+    const subscribe = hello.subscribe(evt => displayLog(evt));
+    const subscribe2 = hello.subscribe(evt => displayLog(evt));
+    subscribe.unsubscribe();
 
     /** end coding */
 }
