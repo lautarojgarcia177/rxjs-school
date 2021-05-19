49c84ca6adf17268f11ea605dd0f3cb8e5ea5dc8
Lautaro Garcia <lautarojgarcia177@gmail.com>
2021-05-19 20:19:12 -0300

    Función from

--- src/create.js
+++ src/create.js
@@ -1,7 +1,16 @@
+import { from } from 'rxjs';
 import { displayLog } from './utils';
 
 export default () => {
     /** start coding */
+    const myArray = [1,2,3,4,5];
+    const myString = 'Hello World';
+    const myPromise = new Promise(resolve => setTimeout(() => {
+        resolve('Hello World')
+    }), 2000);
+
+    const observable = from(myPromise);
+    const subscription = observable.subscribe(val => displayLog(val));
 
     /** end coding */
 }
